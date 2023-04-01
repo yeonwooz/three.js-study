3f3853b41edd54d2dbdc5493757cabb0f8ccfb1a
suyeon woo <woosean999@gmail.com>
2023-04-01 23:29:48 +0900

    01-basic/feat App 클래스 필드추가

--- study/01-basic.js
+++ study/01-basic.js
@@ -6,7 +6,21 @@ class App {
 		this._divContainer = divContainer;
 
 		const renderer = new THREE.WebGLRenderer({ antialias: true });
-		console.log(renderer);
+		renderer.setPixelRatio(window.devicePixelRatio);
+		divContainer.appendChild(renderer.domElement);
+		this._renderer = renderer;
+
+		const scene = new THREE.Scene();
+		this._scene = scene;
+
+		this._setupCamera();
+		this._setupLight();
+		this._setupModel();
+
+		window.onresize = this.resize.bind(this);
+		this.resize();
+
+		requestAnimationFrame(this.render.bind(this));
 	}
 }
 
